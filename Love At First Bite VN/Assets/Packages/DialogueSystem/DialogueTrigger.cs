891386f27028c30e95b7006f35b5b5f96650ea5c
Munenyiwa-88 <munakamune@gmail.com>
2026-04-15 21:53:05 +0200

    DialogueManager and DialogueTrigger
    These are the scripts/classes that the dialogue will be in (Trigger) and the script that will activate it onto the console (Manager).

---
+++ Love At First Bite VN/Assets/Packages/DialogueSystem/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+//Dialogue character class to group the name and icon of a character 
+[System.Serializable]
+public class DialogueCharacter
+{
+    public string name;
+    public Sprite icon;
+}
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+}
