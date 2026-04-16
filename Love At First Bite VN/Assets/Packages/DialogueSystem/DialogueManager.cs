33e864eab3c4e59e00560b12c52f118c5f4b88e8
Munenyiwa-88 <munakamune@gmail.com>
2026-04-16 19:18:58 +0200

    Inserted the dialogue manager
    I inserted the dialogue manager, and I input the text, icon, etc. of the game.

--- Love At First Bite VN/Assets/Packages/DialogueSystem/DialogueManager.cs
+++ Love At First Bite VN/Assets/Packages/DialogueSystem/DialogueManager.cs
@@ -1,16 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
+//The dialoguemanager class will manage all the dialogue inputed into the dialoguetrigger script
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Creating dialogue manager instance, to access class from any point of the game
+    public static DialogueManager Instance;
+
+    //References to UI components, name, icon, dialogue line.
+
+    public Image characterIcon;
+    public TextMeshProUGUI characterName;
+    public TextMeshProUGUI dialogueArea;
+
+
+    private void Start()
     {
-        
+        if (Instance == null)
+            Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
