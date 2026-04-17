a2b4ae9dc0d2cede0f0e22fc2224ffcd7759efa7
Munenyiwa-88 <munakamune@gmail.com>
2026-04-17 22:30:28 +0200

    Finished Dialogue, placed in panels for dialogue, choices, branching, input name typing
    Finished Dialogue, placed in panels for dialogue, choices, branching, input name typing  and finished all of my parts. All that is left is to push to main.

--- Love At First Bite VN/Assets/Packages/DialogueSystem/DialogueManager.cs
+++ Love At First Bite VN/Assets/Packages/DialogueSystem/DialogueManager.cs
@@ -8,48 +8,173 @@ using TMPro;
 //The dialoguemanager class will manage all the dialogue inputed into the dialoguetrigger script
 public class DialogueManager : MonoBehaviour
 {
+    [Header("MainUI")]
     //Calling the main objects for the dialogue
-    public GameObject dialogueBoxPanel;
     public TextMeshProUGUI dialogueText;
     public Button nextButton;
-    //List of strings (List of all of my dialogue lines) - [] - this is what creates the list
-    public string[] sentences; 
-
+    public TMP_InputField nameInputField;
     //The current text number
     public int currentIndex = 0;
+    [Header("Panels")]
+    //Inputting the dialogue choices panel
+    public GameObject choicesPanel1;
+    public GameObject choicesPanel2;
+    public GameObject dialoguePanel;
+    public GameObject nameInputPanel;
+    [Header("Backgrounds")]
+    public Image background2Panel;
+    public Sprite background2Image;
+    [Header("Player Name")]
+    //Input player name typing method
+    private string playerName;
+    //List of strings (List of all of my dialogue lines) - [] - this is what creates the list
+    [Header("Sentences List")]
+    public string[] sentences;
+
+
+
+
+
 
     public void Start()
     {
+        currentIndex = 0;
+        dialogueText.text = sentences[currentIndex];
         //set the text and current index to the first sentence
+        if (currentIndex > 0)
+        {
+            UpdateUI();
+        }
+    }
+    public void Update()
+    {
+        //the line of code that actually changes the image, it checks if the index is right then changes the image
+        if (currentIndex == 5)
+        {
+            background2Panel.sprite = background2Image;
+        }
+    }
+
+
+    public void ShowNextSentence()
+    {
+       
+        //If player chooses option 1 start at 6 or option 2 at 7 then send both to 9
+        // the two lines - || mean or
+        // == means asking/checking = means setting the value 
+        if (currentIndex == 6 || currentIndex == 7)
+        {
+            currentIndex = 9;
+            UpdateUI();
+            //return means to cut the method here. So will not continue to 10 onwards. 
+            return;
+        }
+        //If the player chooses option 3 at 8 send them to 10
+        if (currentIndex == 8)
+        {
+            currentIndex = 10;
+            UpdateUI();
+            return;
+        }
+        //If the player is on Index 9 skip 10 and go to 11
+        if (currentIndex == 9)
+        {
+            currentIndex = 11;
+            UpdateUI();
+            return;
+        }
+       //If we are at 20, end of Yes path || 27 end of No path
+        if (currentIndex == 20 || currentIndex == 27)
+        {
+            EndGame();
+            return;
+        }
+
+        //check if we aren't at the end of the array/list
         if (currentIndex < sentences.Length - 1)
         {
             currentIndex++;
             UpdateUI();
         }
+        else
+        {
+            EndGame();
+        }
 
     }
-    public void Update()
+
+    //Method for different branching dialogues. Connecting choice to responses
+    //Adding parameters, to help jump to different responses in dialogue
+    public void MakeChoice(int jumpToIndex)
     {
-        
+        currentIndex = jumpToIndex;
+        UpdateUI();
     }
 
     private void UpdateUI()
     {
-        dialogueText.text = sentences[currentIndex];
+        //so we have a new variable =currentText, current text takes the original sentence - searches for {Name} and if its there it replaces it with the player name variable
+        string currentText = sentences[currentIndex].Replace("{Name}", playerName);
+        //This is how we convert the sentence itnto the speak text box.
+        dialogueText.text = currentText;
+        //reset all the panels so that they do not overlap
+        nameInputPanel.SetActive(false);
+        choicesPanel1.SetActive(false);
+        choicesPanel2.SetActive(false);
+        nextButton.gameObject.SetActive(true);
+
+
+        //Events- For typing and choices
+        //Will Ask for a name at index 1
+        if (currentIndex == 1)  
+        {
+            nextButton.gameObject.SetActive(false);
+            nameInputPanel.SetActive(true);
+        }
+
+        //First branching at index 5
+       else if (currentIndex == 5)
+        {
+            nextButton.gameObject.SetActive(false);
+            choicesPanel1.SetActive(true);
+        }
+
+        //Second branching at index 17
+        else if (currentIndex == 17)
+        {
+            nextButton.gameObject.SetActive(false);
+            choicesPanel2.SetActive(true);
+        }
+
+        /*
+        //will reuse/bring back - if you deactivate it the dialogue lines will not appear in gameview
+        
         //We will check if we have reached the very last sentence
         if (currentIndex == sentences.Length - 1)
         {
             //activate play button and deactivate next button
+            //load another scene;
         }
         else
         {
             //Keep next button 
         }
+        */
     }
 
-    private void StartGame()
+    public void confirmName()
     {
-        Debug.Log("The game has started");
+        //exclamation marks turns the entire logic negetive, its basically the opposite pf what will be in teh brackets 
+        if (!string.IsNullOrEmpty(nameInputField.text))
+        {
+            playerName = nameInputField.text;
+        }     
+        ShowNextSentence();
+    }
+
+    private void EndGame()
+    {
+        Debug.Log("Game has ended");
 
         //we are gonna hide the ui
     }
