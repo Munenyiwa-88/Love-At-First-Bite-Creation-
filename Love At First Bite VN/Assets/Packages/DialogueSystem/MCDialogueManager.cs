46aa58f747a8ba255715e0afd6d0a01d7ce382af
Munenyiwa-88 <munakamune@gmail.com>
2026-05-01 12:11:35 +0200

    New dialogue system for man
    Created a dialogue system for Monsieur Chocolat

--- Love At First Bite VN/Assets/Packages/DialogueSystem/MCDialogueManager.cs
+++ Love At First Bite VN/Assets/Packages/DialogueSystem/MCDialogueManager.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-
-
-//The dialoguemanager class will manage all the dialogue inputed into the dialoguetrigger script
-public class DialogueManager : MonoBehaviour
+public class MCDialogueManager : MonoBehaviour
 {
     [Header("MainUI")]
     //Calling the main objects for the dialogue
@@ -69,7 +66,7 @@ public class DialogueManager : MonoBehaviour
         characterAngryImage.SetActive(false);
         characterFlirtyImage.SetActive(false);
         //dialoguePlayerBoxImage.SetActive(false);
-        
+
         heartMeterfullImage.SetActive(false);
         heartMeter25Image.SetActive(false);
         heartMeter75Image.SetActive(false);
@@ -82,10 +79,10 @@ public class DialogueManager : MonoBehaviour
         GirlDisgust.Stop();
         GirlGiggle.Play();
         ButtonFeedback.Stop();
-        HeartMeterIncrease.Stop();  
+        HeartMeterIncrease.Stop();
         HeartMeterDecrease.Stop();
 
-      
+
         currentIndex = 0;
         dialogueText.text = sentences[currentIndex];
         //set the text and current index to the first sentence
@@ -186,7 +183,7 @@ public class DialogueManager : MonoBehaviour
         //the line of code that will change the chaarcters heartmeter, based off dialogue choice
         if (currentIndex == 0)
         {
-            heartMeterhalfImage.SetActive(true); 
+            heartMeterhalfImage.SetActive(true);
         }
 
         if (currentIndex == 6 || currentIndex == 7)
@@ -226,7 +223,7 @@ public class DialogueManager : MonoBehaviour
 
     public void ShowNextSentence()
     {
-       
+
         //If player chooses Vanilla start at 6 or Strawberry at 7 then send both to 9
         // the two lines - || mean or
         // == means asking/checking = means setting the value 
@@ -251,7 +248,7 @@ public class DialogueManager : MonoBehaviour
             UpdateUI();
             return;
         }
-       //If we are at 20, end of Yes path || 27 end of No path
+        //If we are at 20, end of Yes path || 27 end of No path
         if (currentIndex == 20 || currentIndex == 27)
         {
             EndGame();
@@ -295,14 +292,14 @@ public class DialogueManager : MonoBehaviour
 
         //Events- For typing and choices
         //Will Ask for a name at index 1
-        if (currentIndex == 1)  
+        if (currentIndex == 1)
         {
             nextButton.gameObject.SetActive(false);
             nameInputPanel.SetActive(true);
         }
 
         //First branching at index 5
-       else if (currentIndex == 5)
+        else if (currentIndex == 5)
         {
             nextButton.gameObject.SetActive(false);
             choicesPanel1.SetActive(true);
@@ -316,14 +313,13 @@ public class DialogueManager : MonoBehaviour
         }
 
     }
-
     public void confirmName()
     {
         //exclamation marks turns the entire logic negetive, its basically the opposite pf what will be in the brackets 
         if (!string.IsNullOrEmpty(nameInputField.text))
         {
             playerName = nameInputField.text;
-        }     
+        }
         ShowNextSentence();
     }
 
@@ -332,290 +328,6 @@ public class DialogueManager : MonoBehaviour
         Debug.Log("Game has ended");
         nextButton.gameObject.SetActive(false);
         SceneManager.LoadScene("Choiceofending");
-
-        //we are gonna hide the ui
     }
-
-
-    /*The things that did not work, with Prototype 1: 
-     1. Beginning commeands for animation 
-    using UnityEngine.Video;
-    using JetBrains.Annotations;
-    
-     2.[Header("CutScenes")]
-     public VideoClip badEndingCutScene;
-     public VideoClip goodEndingCutText;
-     
-     3. [Header("DialogueBox")]
-    public Image dialoguePlayerBoxPanel;
-    public GameObject dialogueRaspberryBoxImage;
-    public GameObject dialoguePlayerBoxImage;
-     
-     4. Heart Meter did not need the SetActive false.
-       - for half image
-            heartMeter75Image.SetActive(false);
-            heartMeter25Image.SetActive(false);
-            heartMeterfullImage.SetActive(false);
-            heartMeteremptyImage.SetActive(false);
-       - for 25 image
-            heartMeterfullImage.SetActive(false);
-            heartMeterhalfImage.SetActive(false);
-            heartMeter75Image.SetActive(false);
-            heartMeteremptyImage.SetActive(false);
-       - for 75 image
-            heartMeter25Image.SetActive(false);
-            heartMeterhalfImage.SetActive(false);
-            heartMeterfullImage.SetActive(false);
-            heartMeteremptyImage.SetActive(false);
-       - for empty image
-            heartMeter75Image.SetActive(false);
-            heartMeter25Image.SetActive(false);
-            heartMeterhalfImage.SetActive(false);
-            heartMeterfullImage.SetActive(false);
-       - for full image
-            heartMeterhalfImage.SetActive(false);
-            heartMeter75Image.SetActive(false);
-            heartMeteremptyImage.SetActive(false);
-            heartMeter25Image.SetActive(false);
-    
-    5. Audio's that we were unable to implement into the game
-       //Setting active the games auadio 
-        if (currentIndex == 0)
-        {
-            ThemeSongBGM.Play();
-            GirlGiggle.Stop();
-            GirlDisgust.Stop();
-            ThemeSongCreepy.Stop();
-            ButtonFeedback.Stop();
-            HeartMeterIncrease.Stop();
-            HeartMeterDecrease.Stop();
-        }
-        if (currentIndex == 2)
-        {
-            GirlGiggle.Play();
-            GirlDisgust.Stop();
-            ThemeSongCreepy.Stop();
-            ButtonFeedback.Stop();
-            HeartMeterIncrease.Stop();
-            HeartMeterDecrease.Stop();
-        }
-    
-        if (currentIndex == 22)
-        {
-            GirlDisgust.Play();
-            ThemeSongBGM.Stop();
-            GirlGiggle.Stop();
-            ButtonFeedback.Stop();
-            HeartMeterIncrease.Stop();
-            HeartMeterDecrease.Stop();
-        }
-
-        if (currentIndex == 22)
-        {
-            GirlDisgust.Play();
-            ThemeSongBGM.Stop();
-            GirlGiggle.Stop();
-            ButtonFeedback.Stop();
-            HeartMeterIncrease.Stop();
-            HeartMeterDecrease.Stop();
-        }
-     
-     6. Wanted to include a player dialogue box, however, the coding glitched and did not work 
-        //coding that will change the dialogue box in accordance to when the player talks
-        if (currentIndex == 1)
-        {
-            dialoguePlayerBoxImage.SetActive(true);
-            dialogueRaspberryBoxImage.SetActive(false); 
-        }
-
-        if (currentIndex == 2)
-        {
-            dialogueRaspberryBoxImage.SetActive(true);
-            dialoguePlayerBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 12)
-        {
-            dialoguePlayerBoxImage.SetActive(true);
-            dialogueRaspberryBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 13)
-        {
-            dialogueRaspberryBoxImage.SetActive(true);
-            dialoguePlayerBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 15)
-        {
-            dialoguePlayerBoxImage.SetActive(true);
-            dialogueRaspberryBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 16)
-        {
-            dialogueRaspberryBoxImage.SetActive(true);
-            dialoguePlayerBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 18)
-        {
-            dialoguePlayerBoxImage.SetActive(true);
-            dialogueRaspberryBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 19)
-        {
-            dialogueRaspberryBoxImage.SetActive(true);
-            dialoguePlayerBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 21)
-        {
-            dialoguePlayerBoxImage.SetActive(true);
-            dialogueRaspberryBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 22)
-        {
-            dialogueRaspberryBoxImage.SetActive(true);
-            dialoguePlayerBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 25)
-        {
-            dialoguePlayerBoxImage.SetActive(true);
-            dialogueRaspberryBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 26)
-        {
-            dialogueRaspberryBoxImage.SetActive(true);
-            dialoguePlayerBoxImage.SetActive(false);
-        }
-
-        if (currentIndex == 27)
-        {
-            dialoguePlayerBoxImage.SetActive(true);
-            dialogueRaspberryBoxImage.SetActive(false);
-        }
-
-     7. Adding a button that would lead to cutscenes.
-     //Condition for the next button to change into an end button and show the cutscenes
-        //If current index == 27 { next button false then set active the next button that will play video}
-
-        /*void DownloadManager VideoClip;{
-
-    }
-
-        
-        if (currentIndex == 27)
-        {
-            endButton.gameObject.SetActive(true);
-            nextButton.gameObject.SetActive(false);
-        }
-
-        if (currentIndex == 20)
-        {
-            endButton.gameObject.SetActive(true);
-            nextButton.gameObject.SetActive(false);
-        }
-
-     8. //public void videobutton () SceneManager.loadscene
-
-        
-     9. //will reuse/bring back - if you deactivate it the dialogue lines will not appear in gameview
-        
-        //We will check if we have reached the very last sentence
-        if (currentIndex == sentences.Length - 1)
-        {
-            //activate play button and deactivate next button
-            //load another scene;
-        }
-        else
-        {
-            //Keep next button 
-        }
-        
-     */
-
-
-
-
-
-
-
-    ////Creating dialogue manager instance, to access class from any point of the game
-    //public static DialogueManager Instance;
-
-    ////References to UI components, name, icon, dialogue line.
-
-    //public Image characterIcon;
-    //public TextMeshProUGUI characterName;
-    //public TextMeshProUGUI dialogueArea;
-
-    ////Create a queue of dialogue lines, order formation
-    //private Queue<DialogueLine> lines;
-
-    //public bool isDialogueActive = false;
-
-    //public float typingSpeed = 0.2f;
-
-    //private void Start()
-    //{
-    //    if (Instance == null)
-    //        Instance = this;
-
-    //}
-
-    ////Adding a startdialogue function that will update the state, play an animation and clear queue
-    //public void StartDialogue(Dialogue dialogue)
-    //{
-    //    isDialogueActive = true;
-
-    //    lines.Clear();
-
-    //    foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
-    //    {
-    //        lines.Enqueue(dialogueLine);
-    //    }
-
-    //    DisplayNextDialogueLine();
-    //}
-
-    ////A display dialogue line to assist with the dequeing of lines in relation to the sprites & text
-    //public void DisplayNextDialogueLine()
-    //{
-    //    if (lines.Count == 28)
-    //    {
-    //        EndDialogue();
-    //        return;
-    //    }
-
-    //    DialogueLine currentLine = lines.Dequeue();
-
-    //    characterIcon.sprite = currentLine.character.icon;
-    //    characterName.text = currentLine.character.name;
-
-    //    StopAllCoroutines();
-
-    //    StartCoroutine(TypeSentence(currentLine));
-    //}
-
-    //// Typesentence method for empty strings and adding characters to them during animation
-    //IEnumerator TypeSentence(DialogueLine dialogueLine)
-    //{
-    //    dialogueArea.text = "";
-    //    foreach (char letter in dialogueLine.line.ToCharArray())
-    //    {
-    //        dialogueArea.text += letter;
-    //        yield return new WaitForSeconds(typingSpeed);
-    //    }
-    //}
-
-    //void EndDialogue()
-    //{
-    //    isDialogueActive = false;
-    //}
 
 }
